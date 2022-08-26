e4e6b3ae8dc743dad38d6d92b4f38e9908cac904
2_legit <111446758+2-legit@users.noreply.github.com>
2022-08-26 15:47:52 +0100

    Update garbage.lua

--- garbage.lua
+++ garbage.lua
@@ -1,1 +1,1 @@
-if not shared.Garbage then loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/GarbageHubMain.lua"))() return end
+if not shared.Garbage then loadstring(game:HttpGet("https://raw.githubusercontent.com/2-legit/GarbageHubForRoblox/main/GarbageHubMain.lua"))() return end
